cb68cec67a7d9bab7cf2ad3da1b6075a386eff78
jandzouana <jandzouana@alcacruz.com>
2024-06-14 20:19:45 -0700

    improved styling for landing

--- src/app/components/landing/Landing.tsx
+++ src/app/components/landing/Landing.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./landing.css";
 import Me from "../../assets/icons/avatar-1.svg";
 import Image from "next/image";
-import HeaderSocials from "./HeaderSocials";
+import LandingSocials from "./LandingSocials";
 import ScrollDown from "./ScrollDown";
 import {aboutBlurb} from "@/app/constants";
 
@@ -14,7 +14,7 @@ const Landing = () => {
                     <h1 className="landing__header">Hi, it's <span className="landing__name">Jessica Andzouana</span></h1>
                     <h3 className="text-animation">I&#39;m a <span></span></h3>
                     <p className="about__blurb">{aboutBlurb}</p>
-                    <HeaderSocials/>
+                    <LandingSocials/>
                 </div>
                 <Image src={Me} alt="me" className="landing__img"/>
             </div>
---
+++ src/app/components/landing/LandingSocials.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import linkedin from "../../assets/icons/linkedin.svg";
+import github from "../../assets/icons/github.svg";
+import ig from "../../assets/icons/instagram.svg";
+import Image from "next/image";
+
+const LandingSocials = () => {
+    return (
+        <div className="landing__socials">
+            <button className="resume__button btn">Resume</button>
+            <div>
+                <a href="https://linkedin.com/in/jandzouana" className="home__social-link" target="_blank">
+                    <Image className={"social-icons"} alt="linkedin" src={linkedin}/>
+                </a>
+                <a href="https://github.com/jandzouana" className="home__social-link" target="_blank">
+                    <Image className={"social-icons"} alt="github" src={github}/>
+                </a>
+                <a href="https://instagram.com/animatedjess" className="home__social-link" target="_blank">
+                    <Image className={"social-icons"} alt="instagram" src={ig}/>
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default LandingSocials
