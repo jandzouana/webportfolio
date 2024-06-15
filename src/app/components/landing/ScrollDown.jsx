32bc7717987c06a6ac4107892d1c714d710631fc
jandzouana <jandzouana@alcacruz.com>
2024-06-14 22:57:02 -0700

    Added mouse and fixed landing page sizing

--- src/app/components/landing/ScrollDown.jsx
+++ src/app/components/landing/ScrollDown.jsx
@@ -4,7 +4,7 @@ function ScrollDown() {
     return (
         <div className="scroll__down">
             <a href="#about" className="mouse__wrapper">
-                <span className="home__scroll-name">Scroll Down</span>
+                <span className="landing__scroll-name">Scroll Down</span>
                 <span className="mouse">
                     <span className="wheel"></span>
                 </span>
