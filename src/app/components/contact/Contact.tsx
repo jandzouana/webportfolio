f95ceb2f1fe4a9f695f9edbea606d2c064e63958
jandzouana <jandzouana@alcacruz.com>
2024-06-18 15:03:13 -0700

    ui improvements to contact form

--- src/app/components/contact/Contact.tsx
+++ src/app/components/contact/Contact.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useFormData } from 'herotofu-react';
 import "./contact.css";
 
-const Contact: React.FC<{}> = () => {
+const Contact: React.FC = () => {
     const { formState, getFormSubmitHandler } = useFormData('https://public.herotofu.com/v1/b1bdc0d0-2db8-11ef-a512-2d50852e3019');
+    const [formValues, setFormValues] = useState({
+        name: '',
+        email: '',
+        subject: '',
+        message: ''
+    });
+    const [formStatus, setFormStatus] = useState<'idle' | 'success' | 'error' | 'loading'>('idle');
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setFormStatus('idle');
+        setFormValues(prevValues => ({
+            ...prevValues,
+            [name]: value
+        }));
+    };
+
+    // const checkFormIsEmpty = () => {
+    //     return !formValues.name && !formValues.email && !formValues.message && !formValues.subject;
+    // }
 
     const onSubmitCallback = ({ status, data }) => {
         console.log(`The form finished submission with status: ${status} and data: ${JSON.stringify(data)}`);
+        if (status === 'success') {
+            setFormValues({
+                name: '',
+                email: '',
+                subject: '',
+                message: ''
+            });
+            setFormStatus('success');
+        } else {
+            setFormStatus('error');
+        }
     };
 
     return (
@@ -14,8 +45,8 @@ const Contact: React.FC<{}> = () => {
             <h2 className="section__title">Get in Touch</h2>
             <div className="contact__container grid">
                 <div className="contact__info">
-                    <h3 className="contact__title">Let's chat!</h3>
-                    <p className="contact__details">Don't like forms? Let's talk on LinkedIn!</p>
+                    <h3 className="contact__title">Let&apos;s chat!</h3>
+                    <p className="contact__details">Don&apos;t like forms? Let&apos;s talk on LinkedIn!</p>
                 </div>
                 <form onSubmit={getFormSubmitHandler(onSubmitCallback)} className="contact__form">
                     <div className="contact__form-group">
@@ -25,7 +56,10 @@ const Contact: React.FC<{}> = () => {
                                 className="contact__form-input"
                                 placeholder="Name"
                                 name="name"
+                                value={formValues.name}
+                                onChange={handleInputChange}
                                 required
+                                disabled={formState.status === 'loading'}
                             />
                         </div>
                         <div className="contact__form-div">
@@ -34,7 +68,10 @@ const Contact: React.FC<{}> = () => {
                                 className="contact__form-input"
                                 placeholder="Email"
                                 name="email"
+                                value={formValues.email}
+                                onChange={handleInputChange}
                                 required
+                                disabled={formState.status === 'loading'}
                             />
                         </div>
                     </div>
@@ -45,7 +82,10 @@ const Contact: React.FC<{}> = () => {
                             className="contact__form-input"
                             placeholder="Subject"
                             name="subject"
+                            value={formValues.subject}
+                            onChange={handleInputChange}
                             required
+                            disabled={formState.status === 'loading'}
                         />
                     </div>
                     <div className="contact__form-div contact__form-area">
@@ -55,12 +95,22 @@ const Contact: React.FC<{}> = () => {
                             className="contact__form-input"
                             placeholder="Write your message here"
                             name="message"
+                            value={formValues.message}
+                            onChange={handleInputChange}
                             required
+                            disabled={formState.status === 'loading'}
                         ></textarea>
                     </div>
-                    <button className="btn contact__button" type="submit">
-                        {formState.status === 'loading' ? 'Sending...' : formState.status === 'success' ? 'Message Sent' : 'Send Message'}
+                    {formStatus === 'success' && <div className="contact-message thank-you-message">Thank you! Your message has been sent.</div>}
+                    {formStatus === 'error' && <div className="contact-message error-message">There was an error submitting your message. Please try again.</div>}
+                    <button
+                        className="btn contact__button"
+                        type="submit"
+                        disabled={formState.status === 'loading'}
+                    >
+                        {formState.status === 'loading' ? 'Sending...' : 'Send Message'}
                     </button>
+
                 </form>
             </div>
         </section>
