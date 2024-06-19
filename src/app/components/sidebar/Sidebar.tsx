28045126ee6b6da433f5b39373d86034f2b6adca
jandzouana <jandzouana@alcacruz.com>
2024-06-18 17:52:25 -0700

    added mobile menu

--- src/app/components/sidebar/Sidebar.tsx
+++ src/app/components/sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import "./sidebar.css";
 import Logo from "./../../assets/logo/logo.png"
 import Image from "next/image";
@@ -8,47 +8,81 @@ interface Props{
 }
 
 const Sidebar : React.FC<Props> = () => {
-    return (
-        <aside className='aside'>
-            <a href="#home" className="nav__logo">
-                <Image src={Logo} alt="Logo"/>
-            </a>
-            <nav className="nav">
-                <div className="nav__menu">
-                    <ul className="nav__list">
-                        <li className="nav__item">
-                            <a href="#home" className="nav__link">
-                                <i className="icon-home"></i>
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="#skills" className="nav__link">
-                                <i className="icon-user"></i>
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="#experience" className="nav__link">
-                                <i className="icon-briefcase"></i>
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="#projects" className="nav__link">
-                                <i className="icon-layers"></i>
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="#contact" className="nav__link">
-                                <i className="icon-bubble"></i>
-                            </a>
-                        </li>
-                    </ul>
+    const [isMobile, setIsMobile] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
+    useEffect(() => {
+        // Function to check screen width and update state
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768); // Adjust the width as per your mobile breakpoint
+        };
+
+        // Initial check on component mount
+        handleResize();
+
+        // Listen to window resize events
+        window.addEventListener('resize', handleResize);
+
+        // Clean up the event listener
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    useEffect(()=>{
+        console.log("Open: " + isOpen);
+    }, [isOpen])
+
+    const handleMenuClick = () => {
+        console.log("Click");
+        setIsOpen(prev => !prev);
+    }
+    return (
+        <>
+            {isMobile && (
+                <div className="nav__menu__icon" onClick={handleMenuClick}>
+                    <i className={`${isOpen ? "icon-menu" : "icon-menu"}`}></i>
                 </div>
-            </nav>
-            <div className="nav__footer">
-                <span className="copyright">Copyright 2024 &copy; Jessica Andzouana</span>
-            </div>
-        </aside>
+            )}
+            <aside className={`aside ${isOpen ? "show" : "hide"}`}>
+                <a href="#home" className="nav__logo">
+                    <Image src={Logo} alt="Logo"/>
+                </a>
+                <nav className="nav">
+                    <div className="nav__menu">
+                        <ul className="nav__list">
+                            <li className="nav__item">
+                                <a href="#home" className="nav__link">
+                                    <i className="icon-home"></i>
+                                </a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="#skills" className="nav__link">
+                                    <i className="icon-user"></i>
+                                </a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="#experience" className="nav__link">
+                                    <i className="icon-briefcase"></i>
+                                </a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="#projects" className="nav__link">
+                                    <i className="icon-layers"></i>
+                                </a>
+                            </li>
+                            <li className="nav__item">
+                                <a href="#contact" className="nav__link">
+                                    <i className="icon-bubble"></i>
+                                </a>
+                            </li>
+                        </ul>
+
+                    </div>
+                </nav>
+                <div className="nav__footer">
+                    <span className="copyright">Copyright 2024 &copy; Jessica Andzouana</span>
+                </div>
+            </aside>
+        </>
     )
 }
 
